c77299a8e2b2735ce2b7dd6a1ed11aa8b983153c
bitterulf <bitterulf@users.noreply.github.com>
2018-03-13 10:46:43 +0100

    Fix cors

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+const server = require('express')();
+var PouchDB = require('pouchdb');
+var cors = require('cors');
 
-const express = require('express');
-
-var app = require('express-pouchdb')({
-  mode: 'fullCouchDB'
+var PouchDBInstance = PouchDB.defaults({
+    mode: 'fullCouchDB'
 });
 
-app.setPouchDB(require('pouchdb'));
-app.listen(3000);
+var db = new PouchDBInstance('data');
+
+var corsOptions = {
+    origin: "http://localhost:3030",
+    credentials: true
+};
+
+server.use('/', cors(corsOptions), require('express-pouchdb')(PouchDBInstance));
+
+
+server.listen(3000, function() {
+    console.log('server running');
+});
